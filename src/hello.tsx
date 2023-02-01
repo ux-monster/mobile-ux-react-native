ff062e3db913128d812037066a1ae9d21ec0f649
popomon <popomonclub@gmail.com>
2023-02-01 23:41:06 +0900

    docs: Applied jsdoc sample

---
+++ src/hello.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+function hello(title, author) {
+  return <div>hello</div>;
+}
+
+export default hello;
